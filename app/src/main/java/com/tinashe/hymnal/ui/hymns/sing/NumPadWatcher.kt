b4ce498ce2d5b0bd68af9d98d8572836d029ce34
Tinashe Mzondiwa <tmzon08@gmail.com>
2020-06-22 00:06:52 -0400

    v1 implementation of number pad

---
+++ app/src/main/java/com/tinashe/hymnal/ui/hymns/sing/NumPadWatcher.kt
@@ -0,0 +1,50 @@
+package com.tinashe.hymnal.ui.hymns.sing
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import androidx.core.text.isDigitsOnly
+
+class NumPadWatcher(
+    private val editText: EditText,
+    private val callbacks: Callbacks
+) : TextWatcher {
+
+    interface Callbacks {
+        fun displayValidNumber(number: String)
+        fun onInputEmpty()
+    }
+
+    override fun afterTextChanged(editable: Editable?) {
+        if (editable.isNullOrEmpty()) {
+            callbacks.onInputEmpty()
+        } else {
+            var input = editable.toString().trim()
+            if (input.isDigitsOnly() && input.isNotEmpty()) {
+                if (input.startsWith("0")) {
+                    input = input.substringAfter("0")
+                    if (input.isEmpty()) {
+                        clearText()
+                        callbacks.onInputEmpty()
+                        return
+                    }
+                }
+                callbacks.displayValidNumber(input)
+            } else {
+                clearText()
+            }
+        }
+    }
+
+    private fun clearText() {
+        editText.removeTextChangedListener(this)
+        editText.setText("")
+        editText.addTextChangedListener(this)
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
+}
--- app/src/main/java/com/tinashe/hymnal/ui/hymns/sing/SingHymnsFragment.kt
+++ app/src/main/java/com/tinashe/hymnal/ui/hymns/sing/SingHymnsFragment.kt
@@ -1,5 +1,6 @@
 package com.tinashe.hymnal.ui.hymns.sing
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,6 +8,13 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.text.isDigitsOnly
+import androidx.core.view.children
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.tinashe.hymnal.R
@@ -24,6 +32,11 @@ class SingHymnsFragment : Fragment() {
     private var binding: FragmentSingBinding? = null
 
     private var appBarBehaviour: AppBarBehaviour? = null
+
+    private val numPadContainer: ConstraintLayout? get() = binding?.sheet?.findViewById(R.id.numPadContainer)
+    private val btnBackSpace: View? get() = binding?.sheet?.findViewById(R.id.btnBackSpace)
+    private val txtNumber: EditText? get() = binding?.sheet?.findViewById(R.id.txtNumber)
+    private val btnGo: Button? get() = binding?.sheet?.findViewById(R.id.btnGo)
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -46,6 +59,7 @@ class SingHymnsFragment : Fragment() {
         }.root
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val number = arguments?.getInt(getString(R.string.argument_selected_hymn)) ?: 1
@@ -62,5 +76,81 @@ class SingHymnsFragment : Fragment() {
                 setCurrentItem(number - 1, false)
             }
         }
+
+        binding?.apply {
+            numPadContainer?.children?.forEach {
+                if (it is Button) {
+                    it.setOnClickListener(btnClick)
+                }
+            }
+
+            txtNumber?.apply {
+                val watcher = NumPadWatcher(
+                    this,
+                    object : NumPadWatcher.Callbacks {
+                        override fun displayValidNumber(number: String) {
+                            btnGo?.isInvisible = false
+                        }
+
+                        override fun onInputEmpty() {
+                            btnGo?.isInvisible = true
+                        }
+                    }
+                )
+                txtNumber?.addTextChangedListener(watcher)
+                txtNumber?.setOnTouchListener { _, _ ->
+                    true
+                }
+
+                btnBackSpace?.setOnClickListener {
+                    val length = txtNumber?.text?.length ?: return@setOnClickListener
+                    if (length > 0) {
+                        txtNumber?.text?.delete(length - 1, length)
+                    }
+                }
+            }
+
+            fabNumber.setOnClickListener {
+                txtNumber?.setText("")
+                fabNumber.isExpanded = true
+                scrimOverLay.isVisible = true
+            }
+            scrimOverLay.setOnTouchListener { _, _ ->
+                if (fabNumber.isExpanded) {
+                    hideNumPad()
+                }
+                true
+            }
+        }
+    }
+
+    private val btnClick = View.OnClickListener {
+        if (it.id == R.id.btnGo) {
+            hideNumPad()
+            val number = txtNumber?.text?.toString() ?: return@OnClickListener
+            if (number.isDigitsOnly()) {
+                binding?.viewPager?.setCurrentItem((number.toInt().minus(1)), false)
+            }
+        } else if (it is Button) {
+            txtNumber?.text?.append(it.text)
+        }
+    }
+
+    private fun hideNumPad() {
+        binding?.apply {
+            scrimOverLay.isVisible = false
+            fabNumber.isExpanded = false
+        }
+    }
+
+    fun didHandleBackPress(): Boolean {
+        return binding?.let {
+            if (it.fabNumber.isExpanded) {
+                hideNumPad()
+                true
+            } else {
+                false
+            }
+        } ?: false
     }
 }
