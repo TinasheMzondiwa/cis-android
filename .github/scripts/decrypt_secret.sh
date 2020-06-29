e246533e54748c4d628e8e9521f64b8dd20da81d
Tinashe Mzondiwa <tmzon08@gmail.com>
2020-06-29 19:54:18 -0400

    Change file directory path

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# Decrypt the file
+mkdir $HOME/secrets
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" \
---output $HOME/app/Christ_key Christ_key.gpg
+--output $HOME/secrets/Christ_key Christ_key.gpg
