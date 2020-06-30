cf45ad4b4722730143212b259b44b486a9c47473
Tinashe Mzondiwa <tmzon08@gmail.com>
2020-06-29 20:22:08 -0400

    Update keystore path

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/cis-android/secrets
+mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" \
---output $HOME/cis-android/secrets/Christ_key Christ_key.gpg
+--output $HOME/secrets/Christ_key Christ_key.gpg
