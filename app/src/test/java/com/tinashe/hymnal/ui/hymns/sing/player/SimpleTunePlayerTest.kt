375d769961e616e438eb6fd8cfcb17fbc159aa67
Tinashe Mzondiwa <tmzon08@gmail.com>
2021-06-05 22:00:18 -0400

    Replace mockito with mockk

--- app/src/test/java/com/tinashe/hymnal/ui/hymns/sing/player/SimpleTunePlayerTest.kt
+++ app/src/test/java/com/tinashe/hymnal/ui/hymns/sing/player/SimpleTunePlayerTest.kt
@@ -6,16 +6,13 @@ import android.content.res.AssetManager
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.tinashe.hymnal.extensions.prefs.HymnalPrefs
 import com.tinashe.hymnal.utils.observeAll
-import org.amshove.kluent.mock
+import io.mockk.every
+import io.mockk.mockk
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.Mockito.`when`
-import org.mockito.junit.MockitoJUnit
-import org.mockito.junit.MockitoRule
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.Shadows.shadowOf
 import org.robolectric.annotation.Config
@@ -28,25 +25,17 @@ import java.io.IOException
 internal class SimpleTunePlayerTest {
 
     @get:Rule
-    val mockitoRule: MockitoRule = MockitoJUnit.rule()
-
-    @get:Rule
     val instantTaskRule = InstantTaskExecutorRule()
 
-    @Mock
-    private lateinit var mockContext: Context
-
-    @Mock
-    private lateinit var mockAssets: AssetManager
-
-    @Mock
-    private lateinit var mockPrefs: HymnalPrefs
+    private val mockContext: Context = mockk()
+    private val mockAssets: AssetManager = mockk()
+    private val mockPrefs: HymnalPrefs = mockk()
 
     private lateinit var tunePlayer: SimpleTunePlayer
 
     @Before
     fun setup() {
-        `when`(mockContext.assets).thenReturn(mockAssets)
+        every { mockContext.assets }.returns(mockAssets)
 
         tunePlayer = SimpleTunePlayer(mockContext, mockPrefs)
     }
@@ -55,8 +44,8 @@ internal class SimpleTunePlayerTest {
     fun `should return true when midi file is available and language code supports media`() {
         // given
         val assetFile = "midis/5.mid"
-        `when`(mockAssets.openFd(assetFile)).thenReturn(mock())
-        `when`(mockPrefs.getSelectedHymnal()).thenReturn("english")
+        every { mockAssets.openFd(assetFile) }.returns(mockk())
+        every { mockPrefs.getSelectedHymnal() }.returns("english")
 
         // when
         val canPlay = tunePlayer.canPlayTune(5)
@@ -69,8 +58,8 @@ internal class SimpleTunePlayerTest {
     fun `should not crash but return false when midi file is un-available`() {
         // given
         val assetFile = "midis/5.mid"
-        `when`(mockAssets.openFd(assetFile)).thenAnswer { throw IOException() }
-        `when`(mockPrefs.getSelectedHymnal()).thenReturn("english")
+        every { mockAssets.openFd(assetFile) }.answers { throw IOException() }
+        every { mockPrefs.getSelectedHymnal() }.returns("english")
 
         // when
         val canPlay = tunePlayer.canPlayTune(5)
@@ -83,8 +72,8 @@ internal class SimpleTunePlayerTest {
     fun `should return false when language code does not support media`() {
         // given
         val assetFile = "midis/5.mid"
-        `when`(mockAssets.openFd(assetFile)).thenReturn(mock())
-        `when`(mockPrefs.getSelectedHymnal()).thenReturn("swahili")
+        every { mockAssets.openFd(assetFile) }.returns(mockk())
+        every { mockPrefs.getSelectedHymnal() }.returns("swahili")
 
         // when
         val canPlay = tunePlayer.canPlayTune(5)
@@ -97,13 +86,13 @@ internal class SimpleTunePlayerTest {
     fun `should emit states ON_PLAY then ON_STOP for an unsuccessful playback`() {
         // given
         val assetFile = "midis/5.mid"
-        val mockFileDescriptor = mock<AssetFileDescriptor>().also {
-            `when`(it.fileDescriptor).thenReturn(mock())
-            `when`(it.startOffset).thenReturn(0L)
-            `when`(it.length).thenReturn(0L)
+        val mockFileDescriptor = mockk<AssetFileDescriptor>().also {
+            every { it.fileDescriptor }.returns(mockk())
+            every { it.startOffset }.returns(0L)
+            every { it.length }.returns(0L)
         }
-        `when`(mockAssets.openFd(assetFile)).thenReturn(mockFileDescriptor)
-        `when`(mockPrefs.getSelectedHymnal()).thenReturn("english")
+        every { mockAssets.openFd(assetFile) }.returns(mockFileDescriptor)
+        every { mockPrefs.getSelectedHymnal() }.returns("english")
         // ShadowDataSource not set-up
         val states = tunePlayer.playbackLiveData.observeAll()
 
@@ -120,10 +109,10 @@ internal class SimpleTunePlayerTest {
     fun `should emit states ON_PLAY then ON_COMPLETE for a successful playback`() {
         // given
         val assetFile = "midis/5.mid"
-        val mockFileDescriptor = mock<AssetFileDescriptor>().also {
-            `when`(it.fileDescriptor).thenReturn(mock())
-            `when`(it.startOffset).thenReturn(0L)
-            `when`(it.length).thenReturn(0L)
+        val mockFileDescriptor = mockk<AssetFileDescriptor>().also {
+            every { it.fileDescriptor }.returns(mockk())
+            every { it.startOffset }.returns(0L)
+            every { it.length }.returns(0L)
         }
         val source = DataSource.toDataSource(
             mockFileDescriptor.fileDescriptor,
@@ -131,8 +120,8 @@ internal class SimpleTunePlayerTest {
             mockFileDescriptor.length
         )
         ShadowMediaPlayer.addMediaInfo(source, ShadowMediaPlayer.MediaInfo())
-        `when`(mockAssets.openFd(assetFile)).thenReturn(mockFileDescriptor)
-        `when`(mockPrefs.getSelectedHymnal()).thenReturn("english")
+        every { mockAssets.openFd(assetFile) }.returns(mockFileDescriptor)
+        every { mockPrefs.getSelectedHymnal() }.returns("english")
         val states = tunePlayer.playbackLiveData.observeAll()
 
         // when
@@ -151,10 +140,10 @@ internal class SimpleTunePlayerTest {
     fun `should emit states ON_PLAY then ON_STOP for a successful playback`() {
         // given
         val assetFile = "midis/5.mid"
-        val mockFileDescriptor = mock<AssetFileDescriptor>().also {
-            `when`(it.fileDescriptor).thenReturn(mock())
-            `when`(it.startOffset).thenReturn(0L)
-            `when`(it.length).thenReturn(0L)
+        val mockFileDescriptor = mockk<AssetFileDescriptor>().also {
+            every { it.fileDescriptor }.returns(mockk())
+            every { it.startOffset }.returns(0L)
+            every { it.length }.returns(0L)
         }
         val source = DataSource.toDataSource(
             mockFileDescriptor.fileDescriptor,
@@ -162,8 +151,8 @@ internal class SimpleTunePlayerTest {
             mockFileDescriptor.length
         )
         ShadowMediaPlayer.addMediaInfo(source, ShadowMediaPlayer.MediaInfo())
-        `when`(mockAssets.openFd(assetFile)).thenReturn(mockFileDescriptor)
-        `when`(mockPrefs.getSelectedHymnal()).thenReturn("english")
+        every { mockAssets.openFd(assetFile) }.returns(mockFileDescriptor)
+        every { mockPrefs.getSelectedHymnal() }.returns("english")
         val states = tunePlayer.playbackLiveData.observeAll()
 
         // when
@@ -181,10 +170,10 @@ internal class SimpleTunePlayerTest {
     fun `should pause media on lifecyle pause`() {
         // given
         val assetFile = "midis/5.mid"
-        val mockFileDescriptor = mock<AssetFileDescriptor>().also {
-            `when`(it.fileDescriptor).thenReturn(mock())
-            `when`(it.startOffset).thenReturn(0L)
-            `when`(it.length).thenReturn(0L)
+        val mockFileDescriptor = mockk<AssetFileDescriptor>().also {
+            every { it.fileDescriptor }.returns(mockk())
+            every { it.startOffset }.returns(0L)
+            every { it.length }.returns(0L)
         }
         val source = DataSource.toDataSource(
             mockFileDescriptor.fileDescriptor,
@@ -192,8 +181,8 @@ internal class SimpleTunePlayerTest {
             mockFileDescriptor.length
         )
         ShadowMediaPlayer.addMediaInfo(source, ShadowMediaPlayer.MediaInfo())
-        `when`(mockAssets.openFd(assetFile)).thenReturn(mockFileDescriptor)
-        `when`(mockPrefs.getSelectedHymnal()).thenReturn("english")
+        every { mockAssets.openFd(assetFile) }.returns(mockFileDescriptor)
+        every { mockPrefs.getSelectedHymnal() }.returns("english")
         val states = tunePlayer.playbackLiveData.observeAll()
 
         // when
@@ -211,10 +200,10 @@ internal class SimpleTunePlayerTest {
     fun `should pause media on toggle`() {
         // given
         val assetFile = "midis/5.mid"
-        val mockFileDescriptor = mock<AssetFileDescriptor>().also {
-            `when`(it.fileDescriptor).thenReturn(mock())
-            `when`(it.startOffset).thenReturn(0L)
-            `when`(it.length).thenReturn(0L)
+        val mockFileDescriptor = mockk<AssetFileDescriptor>().also {
+            every { it.fileDescriptor }.returns(mockk())
+            every { it.startOffset }.returns(0L)
+            every { it.length }.returns(0L)
         }
         val source = DataSource.toDataSource(
             mockFileDescriptor.fileDescriptor,
@@ -222,8 +211,8 @@ internal class SimpleTunePlayerTest {
             mockFileDescriptor.length
         )
         ShadowMediaPlayer.addMediaInfo(source, ShadowMediaPlayer.MediaInfo())
-        `when`(mockAssets.openFd(assetFile)).thenReturn(mockFileDescriptor)
-        `when`(mockPrefs.getSelectedHymnal()).thenReturn("english")
+        every { mockAssets.openFd(assetFile) }.returns(mockFileDescriptor)
+        every { mockPrefs.getSelectedHymnal() }.returns("english")
         val states = tunePlayer.playbackLiveData.observeAll()
 
         // when
@@ -241,10 +230,10 @@ internal class SimpleTunePlayerTest {
     fun `should pause media when new hymn is in view`() {
         // given
         val assetFile = "midis/5.mid"
-        val mockFileDescriptor = mock<AssetFileDescriptor>().also {
-            `when`(it.fileDescriptor).thenReturn(mock())
-            `when`(it.startOffset).thenReturn(0L)
-            `when`(it.length).thenReturn(0L)
+        val mockFileDescriptor = mockk<AssetFileDescriptor>().also {
+            every { it.fileDescriptor }.returns(mockk())
+            every { it.startOffset }.returns(0L)
+            every { it.length }.returns(0L)
         }
         val source = DataSource.toDataSource(
             mockFileDescriptor.fileDescriptor,
@@ -252,8 +241,9 @@ internal class SimpleTunePlayerTest {
             mockFileDescriptor.length
         )
         ShadowMediaPlayer.addMediaInfo(source, ShadowMediaPlayer.MediaInfo())
-        `when`(mockAssets.openFd(assetFile)).thenReturn(mockFileDescriptor)
-        `when`(mockPrefs.getSelectedHymnal()).thenReturn("english")
+        every { mockAssets.openFd(assetFile) }.returns(mockFileDescriptor)
+        every { mockAssets.openFd("midis/6.mid") }.returns(mockFileDescriptor)
+        every { mockPrefs.getSelectedHymnal() }.returns("english")
         val states = tunePlayer.playbackLiveData.observeAll()
 
         // when
