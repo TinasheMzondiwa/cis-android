71bee075d953181f637f7cb783118383532471fe
Tinashe Mzondiwa <tmzon08@gmail.com>
2022-09-15 01:25:16 -0400

    Remove Firebase analytics

--- app/src/main/kotlin/com/tinashe/hymnal/ui/hymns/sing/SingHymnsViewModel.kt
+++ app/src/main/kotlin/com/tinashe/hymnal/ui/hymns/sing/SingHymnsViewModel.kt
@@ -1,15 +1,11 @@
 package com.tinashe.hymnal.ui.hymns.sing
 
-import androidx.core.os.bundleOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.firebase.analytics.FirebaseAnalytics
 import com.tinashe.hymnal.data.model.Hymn
 import com.tinashe.hymnal.data.model.Hymnal
-import com.tinashe.hymnal.data.model.constants.Status
-import com.tinashe.hymnal.extensions.arch.SingleLiveEvent
 import com.tinashe.hymnal.extensions.arch.asLiveData
 import com.tinashe.hymnal.repository.HymnalRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -19,12 +15,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SingHymnsViewModel @Inject constructor(
-    private val repository: HymnalRepository,
-    private val firebaseAnalytics: FirebaseAnalytics
+    private val repository: HymnalRepository
 ) : ViewModel() {
-
-    private val mutableViewState = SingleLiveEvent<Status>()
-    val statusLiveData: LiveData<Status> = mutableViewState.asLiveData()
 
     private val mutableHymnal = MutableLiveData<String>()
     val hymnalTitleLiveData: LiveData<String> = mutableHymnal.asLiveData()
@@ -35,7 +27,6 @@ class SingHymnsViewModel @Inject constructor(
     fun loadData(collectionId: Int) = viewModelScope.launch {
         if (collectionId == -1) {
             repository.getHymns().collectLatest { resource ->
-                mutableViewState.postValue(resource.status)
                 resource.data?.let {
                     mutableHymnsList.postValue(it.hymns)
                     mutableHymnal.postValue(it.title)
@@ -43,7 +34,6 @@ class SingHymnsViewModel @Inject constructor(
             }
         } else {
             val resource = repository.getCollection(collectionId)
-            mutableViewState.postValue(resource.status)
             resource.data?.let {
                 mutableHymnsList.postValue(it.hymns)
                 mutableHymnal.postValue(it.collection.title)
@@ -57,22 +47,11 @@ class SingHymnsViewModel @Inject constructor(
         }
         viewModelScope.launch {
             repository.getHymns(hymnal).collectLatest { resource ->
-                mutableViewState.postValue(resource.status)
                 resource.data?.let {
                     mutableHymnsList.postValue(it.hymns)
                     mutableHymnal.postValue(it.title)
                 }
             }
         }
-    }
-
-    fun hymnViewed(hymn: Hymn) {
-        firebaseAnalytics.logEvent(
-            "HYMN_VIEW",
-            bundleOf(
-                "title" to hymn.title,
-                "hymnal" to hymn.book
-            )
-        )
     }
 }
